c2be3a21ba00f7208a7994cd75018f597cbcaa48
Minf97 <287728237@qq.com>
2023-11-30 11:49:43 +0800

    路径处理

--- packages/mortal-cli/bin/index.js
+++ packages/mortal-cli/bin/index.js
@@ -1,24 +1,35 @@
 #!/usr/bin/env node
 
-
 const yargs = require('yargs');
-const { inquirerPrompt } = require('./inquirer')
+const path = require('path');
+const { inquirerPrompt } = require("./inquirer");
+const { copyDir, checkMkdirExists } = require("./copy");
 
 yargs.command(
-    ['create', 'c'],
-    '新建一个模板',
-    (yargs) => {
-        return yargs.option('name', {
-            alias: 'n',
-            demand: true,
-            describe: '模板名称',
-            demandOption: true,
-            type: 'string'
-        })
-    },
-    (argv) => {
-        inquirerPrompt(argv).then(answers => {
-            console.log('answers', answers);
-        })
-    }
-).argv
+  ['create', 'c'],
+  '新建一个模板',
+  function (yargs) {
+    return yargs.option('name', {
+      alias: 'n',
+      demand: true,
+      describe: '模板名称',
+      type: 'string'
+    })
+  },
+  function (argv) {
+    inquirerPrompt(argv).then(answers => {
+      const { name, type } = answers;
+      const isMkdirExists = checkMkdirExists(
+        path.resolve(process.cwd(),`./src/pages/${name}`)
+      );
+      if (isMkdirExists) {
+        console.log(`${name}文件夹已经存在`)
+      } else {
+        copyDir(
+          path.resolve(__dirname, `./template/${type}`),
+          path.resolve(process.cwd(), `./src/pages/${name}`)
+        )
+      }
+    })
+  }
+).argv;
